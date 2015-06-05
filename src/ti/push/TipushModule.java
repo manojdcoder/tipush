bec5c15f3b888085d1ed93b2cfd67e0045a68ae7
Manoj Kumar <mano.mykingdom@gmail.com>
2015-06-05 14:03:05 +0530

    updated permissions

--- src/ti/push/TipushModule.java
+++ src/ti/push/TipushModule.java
@@ -20,12 +20,9 @@ import org.appcelerator.kroll.common.TiConfig;
 public class TipushModule extends KrollModule
 {
 
-	// Standard Debugging variables
-	private static final String LCAT = "TipushModule";
-	private static final boolean DBG = TiConfig.LOGD;
+	private static final String TAG = "TipushModule";
 
-	// You can define constants with @Kroll.constant, for example:
-	// @Kroll.constant public static final String EXTERNAL_NAME = value;
+	@Kroll.constant public static final String EXTERNAL_NAME = "";
 
 	public TipushModule()
 	{
@@ -35,31 +32,7 @@ public class TipushModule extends KrollModule
 	@Kroll.onAppCreate
 	public static void onAppCreate(TiApplication app)
 	{
-		Log.d(LCAT, "inside onAppCreate");
-		// put module init code that needs to run when the application is created
+		Log.d(TAG, "inside onAppCreate");
 	}
-
-	// Methods
-	@Kroll.method
-	public String example()
-	{
-		Log.d(LCAT, "example called");
-		return "hello world";
-	}
-
-	// Properties
-	@Kroll.getProperty
-	public String getExampleProp()
-	{
-		Log.d(LCAT, "get example property");
-		return "hello world";
-	}
-
-
-	@Kroll.setProperty
-	public void setExampleProp(String value) {
-		Log.d(LCAT, "set example property: " + value);
-	}
-
 }
 
